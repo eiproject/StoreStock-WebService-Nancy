9bbdbd1eac7e1834bea86d3801a4c994618b3808
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-07-15 17:53:10 +0700

    remove some unused import

--- StoreStock/Core/Models/Factory/IFactory.cs
+++ StoreStock/Core/Models/Factory/IFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StoreStock.Models {
+﻿namespace StoreStock.Models {
   interface IFactory {
     IStock FactoryStoreStock(string type,
   int id,
