ea9713aa2ac521a48e373137ca1fd192b9632e19
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-07-19 17:14:40 +0700

    remove unused command in bootstrap

--- StoreStock/Services/CustomBootstrapper/CustomBootstrapper.cs
+++ StoreStock/Services/CustomBootstrapper/CustomBootstrapper.cs
@@ -23,14 +23,11 @@ namespace StoreStockWeb.Services {
     protected override void ConfigureApplicationContainer(TinyIoCContainer container) {
       base.ConfigureApplicationContainer(container);
 			container.Register<IStore>(new Store(new List<IStock>(), "Nano Store"));
-
-			ResourceViewLocationProvider.RootNamespaces.Add(
-				Assembly.GetAssembly(typeof(MainModule)), "StoreStockWeb.Services.Views");
     }
 
-    protected override void ApplicationStartup(TinyIoCContainer container, 
+		protected override void ApplicationStartup(TinyIoCContainer container,
 			IPipelines pipelines) {
-      base.ApplicationStartup(container, pipelines);
+			base.ApplicationStartup(container, pipelines);
 
 			// create dummy data 
 			IStore store = container.Resolve<IStore>();
@@ -41,83 +38,6 @@ namespace StoreStockWeb.Services {
 			storeStock.UseDummyData();
 
 			CookieBasedSessions.Enable(pipelines);
-    }
-
-    protected override void ConfigureConventions(NancyConventions conventions)
-		{
-			base.ConfigureConventions(conventions);
-			conventions.StaticContentsConventions.Add(
-				AddStaticResourcePath("/content", Assembly.GetAssembly(
-					typeof(MainModule)), "StoreStockWeb.Services.Views.content"));
-		}
-
-		protected override NancyInternalConfiguration InternalConfiguration
-		{
-			get
-			{
-				return NancyInternalConfiguration.WithOverrides(OnConfigurationBuilder);
-			}
-		}
-
-		void OnConfigurationBuilder(NancyInternalConfiguration x)
-		{
-			x.ViewLocationProvider = typeof(ResourceViewLocationProvider);
-		}
-
-		public static Func<NancyContext, string, Response> AddStaticResourcePath(string requestedPath, Assembly assembly, string namespacePrefix)
-		{
-			return (context, s) =>
-			       	{
-			       		var path = context.Request.Path;
-						if (!path.StartsWith(requestedPath))
-						{
-							return null;
-						}
-
-						string resourcePath;
-						string name;
-
-						var adjustedPath = path.Substring(requestedPath.Length + 1);
-						if (adjustedPath.IndexOf('/') >= 0)
-						{
-							name = Path.GetFileName(adjustedPath);
-							resourcePath = namespacePrefix + "." + adjustedPath.Substring(0, adjustedPath.Length - name.Length - 1).Replace('/', '.');
-						}
-						else
-						{
-							name = adjustedPath;
-							resourcePath = namespacePrefix;
-						}
-						return new EmbeddedFileResponse(assembly, resourcePath, name);
-			       	};
 		}
 	}
 }
-
-/*using Nancy;
-using Nancy.Hosting.Self;
-using System;
-using StoreStockWebServices.BusinessLogic;
-using Nancy.Bootstrapper;
-using Nancy.ViewEngines;
-using Nancy.TinyIoc;
-using System.Reflection;
-
-namespace StoreStockWebServicess {
-  public class CustomBootstrapper : DefaultNancyBootstrapper {
-    protected override NancyInternalConfiguration InternalConfiguration {
-      get {
-        return NancyInternalConfiguration.WithOverrides(OnConfigurationBuilder);
-      }
-    }
-    void OnConfigurationBuilder(NancyInternalConfiguration x) {
-      x.ViewLocationProvider = typeof(ResourceViewLocationProvider);
-    }
-    protected override void ConfigureApplicationContainer(TinyIoCContainer container) {
-      base.ConfigureApplicationContainer(container);
-      ResourceViewLocationProvider.RootNamespaces.Add(
-        Assembly.GetAssembly(typeof(MainModule)), "StoreStockWebServices.Views");
-    }
-  }
-}
-*/
