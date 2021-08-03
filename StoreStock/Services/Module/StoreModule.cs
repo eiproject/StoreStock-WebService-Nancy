e45dc8b7071d1a6e9bfc28221958b70293ea4ad7
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-03 15:18:49 +0700

    Back

--- StoreStock/Services/Module/StoreModule.cs
+++ StoreStock/Services/Module/StoreModule.cs
@@ -3,8 +3,8 @@
 namespace StoreStockWeb.Services {
   public class StoreModule : NancyModule {
     public StoreModule(StoreAPI storeAPI) : base("/store") {
-      Get["/"] = _ => storeAPI.ReadStoreAPI(Response, Request);
-      Put["/"] = _ => storeAPI.UpdateStoreAPI(Response, Request, this);
+      Get["/"] = _ => storeAPI.ReadStore(Response, Request);
+      Put["/"] = _ => storeAPI.UpdateStore(Response, Request, this);
     }
   }
 }
