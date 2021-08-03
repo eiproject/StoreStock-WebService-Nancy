9055e68130ff7971ea0243dd38e8ed3ecf67dff0
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-03 12:34:07 +0700

    Change `_storeData` to `_stocks`

--- StoreStock/Core/Models/Object/Store.cs
+++ StoreStock/Core/Models/Object/Store.cs
@@ -2,18 +2,18 @@
 
 namespace StoreStock.Models {
   public class Store {
-    private List<Stock> _storeData;
+    private List<Stock> _stocks;
     private string _storeName = "no-name";
     private const decimal _maxDiscountPrice = 0.3m; // max discount
     private const decimal _maxAmountThatGetDiscount = 1000m; // max stock that get discount
     public string StoreName { get { return _storeName; } }
     public decimal MaxDiscountPrice { get { return _maxDiscountPrice; } }
     public decimal MaxAmountThatGetDiscount { get { return _maxAmountThatGetDiscount; } }
-    public List<Stock> StoreData { get { return _storeData; } }
+    public List<Stock> StoreData { get { return _stocks; } }
     public int LastID {
       get {
-        if (_storeData.Count > 0) {
-          return _storeData[_storeData.Count - 1].ID;
+        if (_stocks.Count > 0) {
+          return _stocks[_stocks.Count - 1].ID;
         }
         else {
           return -1;
@@ -21,19 +21,19 @@ namespace StoreStock.Models {
       }
     }
     public void AddStock(Stock newStock) {
-      _storeData.Add(newStock);
+      _stocks.Add(newStock);
     }
     public void RemoveStock(Stock stock) {
-      _storeData.Remove(stock);
+      _stocks.Remove(stock);
     }
     public void CleanStock() {
-      _storeData.Clear();
+      _stocks.Clear();
     }
     public void UpdateName(string name) {
       _storeName = name;
     }
     internal Store(string storeName) {
-      _storeData = new List<Stock>();
+      _stocks = new List<Stock>();
       _storeName = storeName;
     }
   }
